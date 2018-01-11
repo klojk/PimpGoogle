30f3bd342e8bc5ceb2b68def820aa96de64cf646
chloejk <chloejk@msn.com>
2018-01-11 18:29:18 +0100

    visibility, alert

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let center= document.getElementById('hide');
+
+let button= document.getElementById('Showbutton');
+
+function hideShow()
+{
+center.style.visibility="visible";
+button.style.visibility="hidden";
+}
+
+setTimeout(function() { alert("Viens voir mon site de boules!"); }, 10000);
